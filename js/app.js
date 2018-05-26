6fb9002658bf8a9a6b5990bc3d3643dde2158d0b
Matthew Cranford <mcranfordmail@gmail.com>
2018-05-26 15:14:11 -0700

    Move cards outside HTML and into JavaScript

--- js/app.js
+++ js/app.js
@@ -2,6 +2,41 @@
  * Create a list that holds all of your cards
  */
 
+const cards = [
+  'fa-diamond',
+  'fa-diamond',
+  'fa-paper-plane-o',
+  'fa-paper-plane-o',
+  'fa-anchor',
+  'fa-anchor',
+  'fa-bolt',
+  'fa-bolt',
+  'fa-cube',
+  'fa-cube',
+  'fa-leaf',
+  'fa-leaf',
+  'fa-bicycle',
+  'fa-bicycle',
+  'fa-bomb',
+  'fa-bomb'
+];
+
+const shuffledCards = shuffle(cards);
+console.log(shuffledCards);
+
+document.querySelector('.deck').innerHTML = shuffledCards
+  .map(card => {
+    return generateCard(card);
+  })
+  .join('');
+
+function generateCard(card) {
+  const newCard = `<li class="card">
+  <i class="fa ${card}"></i>
+</li>`;
+  return newCard;
+}
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -62,7 +97,7 @@ document.querySelector('.deck').addEventListener('click', function(event) {
 function updateMoves() {
   moves++;
   document.querySelector('.moves').textContent = moves;
-  updateStars()
+  updateStars();
 }
 
 function updateStars() {
